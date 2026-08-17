4a60338be32fcc7c81c4a0ee6246fa3e54d88671
Shayab Rahman <shayabrahman354@gmail.com>
2026-08-17 23:11:20 +0530

    project2

---
+++ react-project-two/src/components/UserCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./UserCard.css";
+
+const UserCard = (props) => {
+  return (
+    <div className="user-container" style={props.style}>
+      <p id="user-name">{props.name}</p>
+
+      <img
+        id="user-img"
+        src={props.image}
+        alt={props.name}
+      />
+
+      <p id="user-desc">{props.desc}</p>
+    </div>
+  );
+};
+
+export default UserCard;
