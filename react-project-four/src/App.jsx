d4f3c69301093b5ac38e542213c4bb8cd0c18439
Shayab Rahman <shayabrahman354@gmail.com>
2026-08-20 02:49:47 +0530

    commit

---
+++ react-project-four/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Button from './components/Button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <Button handleClick={handleClick} text="Click me">
+        <h1>{count}</h1>
+      </Button>
+    </div>
+  )
+}
+
+export default App
---
+++ react-project-four/src/components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = (props) => {
+  return (
+    <div>
+      {props.children}
+
+      <button onClick={props.handleClick}>
+        {props.text}
+      </button>
+    </div>
+  )
+}
+
+export default Button
